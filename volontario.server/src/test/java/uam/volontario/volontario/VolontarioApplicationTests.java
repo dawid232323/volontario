b16b5d14b4bd17f30c0937c19e03de1534dd7b30
mmusialowicz <micmus4@st.amu.edu.pl>
2023-03-17 18:20:24 +0100

    VOL-17 - added module with volontario server side, created Spring Boot project with dependencies supporting REST and Security

---
+++ volontario.server/src/test/java/uam/volontario/volontario/VolontarioApplicationTests.java
@@ -0,0 +1,13 @@
+package uam.volontario.volontario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolontarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
