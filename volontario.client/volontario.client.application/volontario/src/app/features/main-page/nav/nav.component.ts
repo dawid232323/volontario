6f8941c9694e711eae2ca73595d46031d8215bc4
Mateusz Cwojdziński <matcwo@st.amu.edu.pl>
2023-05-01 20:40:31 +0200

    Add sidebar toggle handler + change colors for sections svg's (#41)
    Co-authored-by: Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
    Reviewed-on: https://git.wmi.amu.edu.pl/s464956/volontario/pulls/41

--- volontario.client/volontario.client.application/volontario/src/app/features/main-page/nav/nav.component.ts
+++ volontario.client/volontario.client.application/volontario/src/app/features/main-page/nav/nav.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
+  animations: [
+    trigger('sidebarTrigger', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('300ms ease-in', style({ transform: 'translateY(0%)' })),
+      ]),
+      state('open', style({ transform: 'translateX(0%)' })),
+      state('close', style({ transform: 'translateX(-100%)' })),
+      transition('open => close', [animate('300ms ease-in')]),
+      transition('close => open', [animate('300ms ease-out')]),
+    ]),
+  ],
 })
 export class MainPageNavComponent implements OnInit {
+  public showSidebar: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  public toggleMenu(): void {
+    this.showSidebar = !this.showSidebar;
+  }
 }
