9be2eb0a63e3cb153412c877ebafacb9bf44593e
Mikołaj Mumot <mikmum@st.amu.edu.pl>
2023-12-30 16:59:23 +0100

    vol-225 Change CORS configuration (#165)
    Co-authored-by: Mikołaj <mikmum@st.amu.edu.pl>
    Reviewed-on: https://git.wmi.amu.edu.pl/s464956/volontario/pulls/165

--- volontario.server/src/main/java/uam/volontario/configuration/CorsConfiguration.java
+++ volontario.server/src/main/java/uam/volontario/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package uam.volontario.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,14 +11,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer
 {
+    private final String protocol;
+    private final String volontarioHost;
+
+    public CorsConfiguration( final @Value( "${schema}" ) String aProtocol,
+                              final @Value( "${volontarioHost}" ) String aVolontarioHost )
+    {
+        this.protocol = aProtocol;
+        this.volontarioHost = aVolontarioHost;
+    }
+
     @Override
     public void addCorsMappings( CorsRegistry aRegistry )
     {
-        // TODO: Do not forget to check it at some point.
+        String originPattern = protocol + "://" + volontarioHost + ":[*]";
         aRegistry.addMapping( "/**" )
                 .allowedMethods( "POST", "GET", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD" )
                 .allowedHeaders( "Authorization", "Cache-Control", "Content-Type" )
                 .allowCredentials( true )
-                .allowedOriginPatterns( "*" );
+                .allowedOriginPatterns( originPattern );
     }
 }
